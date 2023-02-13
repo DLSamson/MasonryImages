87e276efa5af1eff5c005e5707f5020e95e3a42b
dgarmash <dgarmash@synergy.ru>
2023-02-13 21:29:26 +0300

    Change PonyWeb API to Wishy for valentine day and since PonyWeb API is not working anymore

--- assets/script.js
+++ assets/script.js
@@ -1,92 +1,170 @@
 $(document).ready(() => {
-    const dashboard = $('.dashboard').masonry({
-        columnWidth: '.grid-sizer',
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        transitionDuration: '0.5s',
-    });
+    /**
+     * 
+     * @param getImages function(limit:int, offset:int)  
+     */
+    const initImages = (getImages) => {
+        const dashboard = $('.dashboard').masonry({
+            columnWidth: '.grid-sizer',
+            itemSelector: '.grid-item',
+            percentPosition: true,
+            transitionDuration: '0.5s',
+        });
 
-    dashboard.on('click', '.grid-item', function () {
-        $(this).toggleClass('grid-item-width');
-        dashboard.masonry('layout');
-    });
+        dashboard.on('click', '.grid-item', function () {
+            $(this).toggleClass('grid-item-width');
+            dashboard.masonry('layout');
+        });
 
-    const imageObserver = new IntersectionObserver(
-        (entries, observer) => {
-            entries.forEach((entry) => {
-                if(entry.isIntersecting) {
-                    observer.unobserve(entry.target);
-                    loadImages();
-                }  
-            });
-        },
-        {
-            rootMargin: '300px 0px 0px'
-        }
-    );
+        const imageObserver = new IntersectionObserver((entries, observer) => {
+                entries.forEach((entry) => {
+                    if(entry.isIntersecting) {
+                        observer.unobserve(entry.target);
+                        updateImages();
+                    }  
+                });
+            },
+            {rootMargin: '300px 0px 0px'}
+        );
 
-    const apiUrl = 'https://ponyweb.ml/v1/image/all';
-    let limit = 30;
-    let offset = 0;
+        /**
+         * 
+         * @param images {url, name, comment, id}[]
+         */
+        const addImages = async (images) => {
+            try {
+                images.forEach((image, index) => {
+                    /*
+                    <div class="grid-item grid-item-width">
+                        <img src="" title="" id="">
+                    </div>
+                    */
 
-    const loadImages = async () => {
-        try {
-            const response = await axios.get(
-                apiUrl,
-                {
-                    params: {
-                        limit: limit,
-                        offset: limit * offset++,
-                    }
-                }
-            );
-            const images = response.data.data;
-            images.forEach((image, index) => {
-                /*
-                <div class="grid-item grid-item-width">
-                    <img src="" title="" id="">
-                </div>
-                */
+                    const gridItem = document.createElement('div');
+                    const img = document.createElement('img');
+                    img.src = image.url;
+                    img.title = `${image.name} | ${image.comment}`;
+                    img.id = image.id;
 
-                const gridItem = document.createElement('div');
-                const img = document.createElement('img');
-                img.src = image.url;
-                img.title = `${image.name} | ${image.comment}`;
-                img.id = image.id;
+                    // Not sure about the perfomance
+                    $(img).on('load', () => {
+                        dashboard.masonry('layout');
+                    });
 
-                // Not sure about the perfomance
-                $(img).on('load', () => {
-                    dashboard.masonry('layout');
+                    gridItem.classList.add('grid-item');
+                    index % 5 === 0
+                        ? gridItem.classList.add('grid-item-width')
+                        : null;
+
+                    gridItem.appendChild(img);
+
+                    dashboard
+                        .append(gridItem)
+                        .masonry('appended', gridItem);
                 });
 
-                gridItem.classList.add('grid-item');
-                index % 5 === 0
-                    ? gridItem.classList.add('grid-item-width')
-                    : null;
+                setTimeout(() => {
+                    //In case if masonry messed up while loading
+                    dashboard.masonry('layout');
 
-                gridItem.appendChild(img);
+                    const lastGridItem = $('.dashboard .grid-item:last-child')[0];
+                    imageObserver.observe(lastGridItem);
+                }, 1500);
 
-                dashboard
-                    .append(gridItem)
-                    .masonry('appended', gridItem);
-            });
+            } catch (error) {
+                alert('Something went wrong, cannot load more images');
+                console.error(error);
+            }
 
-            setTimeout(() => {
-                //In case if masonry messed up while loading
-                dashboard.masonry('layout');
+        };
 
-                const lastGridItem = $('.dashboard .grid-item:last-child')[0];
-                imageObserver.observe(lastGridItem);
-            }, 1500);
-
-        } catch (error) {
-            alert('Something went wrong, cannot load more images');
-            console.error(error);
+        let limit = 30;
+        let offset = 0;
+        const updateImages = () => {
+            try {
+                const images = getImages(limit, offset);
+                addImages(images);
+                offset += limit;
+            }
+            catch (error) {
+                alert('Something went wrong, cannot load more images');
+                console.error(error);
+            }
         }
-
+        updateImages();
     };
-    loadImages();
+
+    initImages((limit, offset) => {
+        const cards = [
+            "white-valentine-card",
+            "teddy-bear-valentine-card",
+            "happy-valentine-red-card",
+            "couple-sitting-on-bench",
+            "golden-heart-love",
+            "heart-shape-Gifts-for-valentine",
+            "calender-for-valentine",
+            "wed-couple-in-valentine",
+            "couple-at-coffee-table",
+            "love-mobile-app-card",
+            "fun-on-valentine-day",
+            "glitter-heart-propose",
+            "couple-valentine-calender",
+            "gift-exchange-valentine-card",
+            "heart-gift-valentine-card",
+            "valentine-propose-card",
+            "couple-valentine-date",
+            "happy-valentine-card",
+            "pink-rose-valentine-s-day",
+            "flower-pot-with-red-rose",
+            "red-heart-shape-tree",
+            "dog-with-rose-for-valentine",
+            "beared-guy-love-propose",
+            "teddybear-for-you",
+            "lock-love-propose",
+            "heart-in-crystal-bowl",
+            "love-birds-enjoying-moment",
+            "green-garden-grass-cutting",
+            "propose-leaf-of-wind-chill",
+            "i-love-you-with-pink-sketch"
+        ];
+        const words = [
+            "Daniel",
+            "Honey",
+            "Cutie",
+            "Sunshine",
+            "I'm_under_your_spell",
+            "Darling",
+            "Sweetie",
+            "You're_my_angel",
+            "You're_perfect",
+        ];
+        
+        const images = [];
+
+        for (let index = 0; index < limit; index++) {
+            const card = cards[getRandomInt(cards.length)];
+            const word = words[getRandomInt(words.length)];
+
+            images.push({
+                url: `https://wishiy.com/i/${card}/for/${word}.jpg`,
+                name: '', 
+                comment: '', 
+                id: `${card}-${word}`
+            });
+        }
+        
+        log('Images', images);
+        return images;
+    });
 });
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+function log(...data) {
+    const DEBUG = true;
+    if(DEBUG) console.log(...data);
+}
 
 /* 
     Anyways this code can be laggy 
